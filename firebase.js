f6460ea7da3e6bacbc9640773ad0a0846e746fc4
Nikusha134 <105487411+Nikusha134@users.noreply.github.com>
2025-07-04 02:17:49 +0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxBMrWfE5-2BIdeNFd_5OGI47YC9lm6_s",
+  authDomain: "nikusha-web.firebaseapp.com",
+  projectId: "nikusha-web",
+  storageBucket: "nikusha-web.appspot.com",
+  messagingSenderId: "262614394157",
+  appId: "1:262614394157:web:f9a6e9e7986d3bd8941fa4",
+  measurementId: "G-NGHNKSV255"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
